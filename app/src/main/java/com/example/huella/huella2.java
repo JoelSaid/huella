86fca7111f9f1c1bc15763b1fffa13e3e2dddc1e
JoelSaid <essjoelssm@gmail.com>
2020-12-08 14:34:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/huella/huella2.java
@@ -0,0 +1,14 @@
+package com.example.huella;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class huella2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huella2);
+    }
+}
